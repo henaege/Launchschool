dfb65453bca9b4b2b852cdf2ec1b30dedc4aa008
henaege <henaege@gmail.com>
2016-04-24 11:42:38 -0400

    Added files via upload
    Exercises from the Variables section of Intro to Programming

---
+++ age.rb
@@ -0,0 +1,18 @@
+puts 'How old are you?'
+age = gets.chomp
+
+puts 'In 10 years you will be:'
+age_plus_10 = age.to_i + 10
+puts age_plus_10
+
+puts 'In 20 years you will be:'
+age_plus_20 = age.to_i + 20
+puts age_plus_20
+
+puts 'In 30 years you will be:'
+age_plus_30 = age.to_i + 30
+puts age_plus_30
+
+puts 'In 40 years you will be:'
+age_plus_40 = age.to_i + 40
+puts age_plus_40
---
+++ name.rb
@@ -0,0 +1,13 @@
+puts 'Please enter your first name:'
+first_name = gets.chomp
+
+puts 'Hello ' + first_name + ', it\'s nice to meet you.'
+
+10.times do
+  puts first_name
+end
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts first_name + ' ' + last_name
